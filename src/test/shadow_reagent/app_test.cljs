06e86714e6076acb80fa439a8fd8cc9a237cedca
Bobbi Towers <btowers793@gmail.com>
2023-05-29 12:22:28 -0700

    Initial commit

---
+++ src/test/shadow_reagent/app_test.cljs
@@ -0,0 +1,10 @@
+(ns shadow-reagent.app-test
+  (:require [cljs.test :refer [deftest is run-tests]]
+            [shadow-reagent.app :as app]))
+
+(deftest my-test
+  )
+
+(comment
+  (run-tests)
+  )
